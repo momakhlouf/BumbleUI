a2257cb768b323ff6475fe32bb613cf00b82beb9
Mohamed Makhlouf <gaaarw@gmail.com>
2024-05-22 15:35:31 +0300

    [Feature] Add home view header

---
+++ BumbleUI/Core/View/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BumbleUI
+//
+//  Created by Mohamed Makhlouf Ahmed on 22/05/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
