69990acdbcd15f186a3eb6fa0d29a1a5a58540c2
Mohamed Makhlouf <gaaarw@gmail.com>
2024-05-22 15:36:07 +0300

    [Feature] Add home view header

--- BumbleUI/Core/View/Home/HeaderView.swift
+++ BumbleUI/Core/View/Home/HeaderView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            HStack(spacing: 0){
+            Image(systemName: "line.horizontal.3")
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+                .onTapGesture {
+                    
+                }
+            
+            Image(systemName: "arrow.uturn.left")
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+        }
+            Text("bumble")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.bumbleYellow)
+                .font(.title)
+            
+            Image(systemName: "slider.horizontal.3")
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+        }
+        .font(.title2)
+        .fontWeight(.medium)
+        .foregroundStyle(.bumblBlack)
+        .padding(.horizontal, 8)
     }
 }
 
--- BumbleUI/Core/View/Home/HomeView.swift
+++ BumbleUI/Core/View/Home/HomeView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.bumbleWhite.ignoresSafeArea()
+            
+            VStack{
+                HeaderView()
+                    .frame(maxHeight: .infinity,alignment: .top)
+            }
+        }
     }
 }
 
