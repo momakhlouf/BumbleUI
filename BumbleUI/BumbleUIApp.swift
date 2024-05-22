40837cc57852d4cd39a9a6680da8ea3aff4dc6b8
Mohamed Makhlouf <gaaarw@gmail.com>
2024-05-22 12:38:57 +0300

    Initial Commit

---
+++ BumbleUI/BumbleUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumbleUIApp.swift
+//  BumbleUI
+//
+//  Created by Mohamed Makhlouf Ahmed on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BumbleUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
